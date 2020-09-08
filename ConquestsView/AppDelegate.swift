a01e1b3bf06d26c87051878255b2c73cfbd05997
Danilo Henrique <danilo.e.souza@prservicos.com.br>
2020-09-07 21:00:47 -0300

    kjandlaskn

--- ConquestsView/AppDelegate.swift
+++ ConquestsView/AppDelegate.swift
@@ -11,13 +11,8 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(handleNotificationCenter), name: Notification.Name.conquestViewDidLoad, object: nil)
-        
+
         return true
     }
 
@@ -33,15 +28,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-        
-        NotificationCenter.default.removeObserver(self, name: Notification.Name.conquestViewDidLoad, object: nil)
-        
     }
-    
-    @objc func handleNotificationCenter() {
-        print("---->")
-    }
-
-
 }
 
--- ConquestsView/Controllers/ConquestViewController.swift
+++ ConquestsView/Controllers/ConquestViewController.swift
@@ -8,94 +8,60 @@
 
 import UIKit
 
-protocol SuperToContainer: AnyObject {
-    func updateContainer(id: Int)
-}
-
-class ConquestViewController: UIViewController, ContainerToSuper {
-    
-    let CONST_ID_POSTAL = 0
-    let CONST_ID_SOUVENIR = 1
-    let CONST_ID_BADGE = 2
-    
-    weak var delegate: SuperToContainer?
+class ConquestViewController: UIViewController {
 
     @IBOutlet weak var postalOutlet: UIButton!
     @IBOutlet weak var souvenirOutlet: UIButton!
     @IBOutlet weak var badgeOutlet: UIButton!
+    @IBOutlet var allButtons: [UIButton]!
+    
+    var conquestPageViewController: ConquestPageViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setSelectedButton(btn: postalOutlet)
-        setUnselectedButton(btn: souvenirOutlet)
-        setUnselectedButton(btn: badgeOutlet)
-        // Do any additional setup after loading the view.
-        
-        NotificationCenter.default.post(name: Notification.Name.conquestViewDidLoad, object: nil)
-        
-    }
-
-    private func enablePostalBtn() {
-        setSelectedButton(btn: postalOutlet)
-        setUnselectedButton(btn: souvenirOutlet)
-        setUnselectedButton(btn: badgeOutlet)
+        setSelectedButton(postalOutlet)
     }
     
-    @IBAction func postalAction(_ sender: Any) {
-        enablePostalBtn()
-        delegate?.updateContainer(id: CONST_ID_POSTAL)
+    @IBAction func didTouchSegmentButton(_ sender: UIButton) {
+        let conquestsViewsControllers = conquestPageViewController.conquestsViewsControllers
+        guard
+            let indexOfSelectedButton = allButtons.firstIndex(of: sender),
+            let viewControllerBeingDisplayed = conquestPageViewController.viewControllers?.first,
+            let indexOfViewControllerBeingDisplayed = conquestsViewsControllers.index(of: viewControllerBeingDisplayed)
+        else { return }
+        setSelectedButton(sender)
+        let viewControllerToDisplay = conquestsViewsControllers[indexOfSelectedButton]
+        let direction: UIPageViewController.NavigationDirection = indexOfSelectedButton > indexOfViewControllerBeingDisplayed ? .forward : .reverse
+        conquestPageViewController.setViewControllers([viewControllerToDisplay],
+            direction: direction,
+            animated: true,
+            completion: nil
+        )
     }
     
-    private func enableSouvenirBtn() {
-        setSelectedButton(btn: souvenirOutlet)
-        setUnselectedButton(btn: postalOutlet)
-        setUnselectedButton(btn: badgeOutlet)
-    }
-    
-    @IBAction func souvenirAction(_ sender: Any) {
-        enableSouvenirBtn()
-        delegate?.updateContainer(id: CONST_ID_SOUVENIR)
-    }
-    
-    private func enableBadgeBtn() {
-        setSelectedButton(btn: badgeOutlet)
-        setUnselectedButton(btn: souvenirOutlet)
-        setUnselectedButton(btn: postalOutlet)
-    }
-    
-    @IBAction func badgeAction(_ sender: Any) {
-        enableBadgeBtn()
-        //superToContainer?.updateContainer(id: CONST_ID_BADGE)
-    }
-    
-    func setSelectedButton(btn: UIButton) {
-        btn.setTitleColor(#colorLiteral(red: 0.9844431281, green: 0.9844661355, blue: 0.9844536185, alpha: 1), for: .normal)
-        btn.backgroundColor = #colorLiteral(red: 0.3111339808, green: 0.1553293169, blue: 0.7954767346, alpha: 1)
-        btn.layer.borderWidth = 0
-    }
-    
-    func setUnselectedButton(btn: UIButton) {
-        btn.setTitleColor(#colorLiteral(red: 0.3111339808, green: 0.1553293169, blue: 0.7954767346, alpha: 1), for: .normal)
-        btn.backgroundColor = #colorLiteral(red: 0.9844431281, green: 0.9844661355, blue: 0.9844536185, alpha: 1)
-        btn.layer.borderWidth = 1
-        btn.layer.borderColor = #colorLiteral(red: 0.3111339808, green: 0.1553293169, blue: 0.7954767346, alpha: 1)
-    }
-    
-    func updateView(id: Int) {
-        if id == CONST_ID_POSTAL {
-            enablePostalBtn()
-        } else if id == CONST_ID_SOUVENIR {
-            enableSouvenirBtn()
-        } else if id == CONST_ID_BADGE {
-            enableBadgeBtn()
-        } else {
-            return
+    private func unselectAllButtons() {
+        for button in allButtons {
+            setUnselectedButton(button)
         }
     }
     
-}
-
-extension Notification.Name {
-    static let conquestViewDidLoad = Notification.Name("conquestViewDidLoad")
+    func setSelectedButton(_ button: UIButton) {
+        unselectAllButtons()
+        button.setTitleColor(#colorLiteral(red: 0.9844431281, green: 0.9844661355, blue: 0.9844536185, alpha: 1), for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.3111339808, green: 0.1553293169, blue: 0.7954767346, alpha: 1)
+        button.layer.borderWidth = 0
+    }
+    
+    func setUnselectedButton(_ button: UIButton) {
+        button.setTitleColor(#colorLiteral(red: 0.3111339808, green: 0.1553293169, blue: 0.7954767346, alpha: 1), for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.9844431281, green: 0.9844661355, blue: 0.9844536185, alpha: 1)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = #colorLiteral(red: 0.3111339808, green: 0.1553293169, blue: 0.7954767346, alpha: 1)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "containerSegue" {
+            conquestPageViewController = segue.destination as? ConquestPageViewController
+        }
+    }
 }
